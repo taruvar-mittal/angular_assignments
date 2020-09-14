437ab14f476b36b6eb7900d5ee95f4943ad43635
taruvar-mittal <mittaltaruvar@gmail.com>
2020-09-14 16:33:37 +0530

    routing start

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,20 +11,12 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
-const appRoutes: Routes = [
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent}
-  ] },
-  { path: '', component: HomeComponent},
-  { path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent},
-      { path: ':id/edit', component: EditServerComponent}
-  ]},
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/not-found'} // should be the last one to be declared
 
-];
 
 @NgModule({
   declarations: [
@@ -41,9 +32,9 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
